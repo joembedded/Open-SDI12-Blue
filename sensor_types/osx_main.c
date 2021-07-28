47e8d65c4da8895840d1d17d4b59c978541a79c7
Jo <joembedded@gmail.com>
2021-07-28 17:02:48 +0200

    Update osx_main.c

--- sensor_types/osx_main.c
+++ sensor_types/osx_main.c
@@ -89,50 +89,81 @@ uint16_t sdi_track_crc16(uint8_t *pdata, uint16_t wlen, uint16_t crc_run) {
 /* Memory Access - Defines SECTOR 1 Sized internal parameter access
   uint16_t crc16; // CRC of parid, qwords and all data bytes
   uint8_t parid;  // Allowed 0..xFE
-  uint8_t qwords; // No of following data (as uint32 (can be 0))
-  uint32 qdata[qwords];
+  uint8_t bytes; // No of following data in bytes (can be 0)
+  uint32 qdata[qwords]; // Allocated words ((Bytes+3)&252)
 
   NRF52832 CPU:  Flash: 0x6F000 / 454656.d
   NRF52840 CPU:  Flash: 0xEF000 / 978944.d
 */
-#define MAX_DW 16 // Maximum No of uint32 Params max. 0xFE
-uint8_t intmem_buf[MAX_DW+1]; // Buffer to IntMem
 
 #define IMTMEM_SIZE   CPU_SECTOR_SIZE // 4096 for NRF52
 #define INTMEM_START  (IBOOT_FLASH_START+IBOOT_FLASH_SIZE-IMTMEM_SIZE) // Located at End of Boot-Memory
 
 // Result >=0: Rel Pos. in INTMEM, <0: ERROR
-int16_t intpar_mem_write(uint8_t parid, uint8_t qwords, uint8_t *pdata){
-  uint16_t plen;  // Len of Parameter Blox in Bytes
-  uint16_t crc;
+int16_t intpar_mem_write(uint8_t parid, uint8_t pbytes, uint8_t *pdata){
+  uint16_t plen;  // Len of Parameter Block in Bytes M4
   uint32_t mem_addr;
-  intmem_buf[2]=parid;
-  intmem_buf[3]=qwords;
-  if(qwords){
-    if(qwords>MAX_DW) return -1;  // Too much data
-    plen=qwords*4;
-    memcpy(intmem_buf+4,pdata,plen);  // DSN
-  }else plen=0;
-
   mem_addr =  INTMEM_START;
   for(;;){
     if ((*(uint32_t*)mem_addr)==0xFFFFFFFF) break;
     if (mem_addr >= (INTMEM_START+IMTMEM_SIZE)) break;
-    mem_addr+= ((*(uint8_t*)(mem_addr+3))*4)+4; // Next Entry
+    plen=((*(uint8_t*)(mem_addr+3))+3)&252;
+    mem_addr+= plen+4; // Next Entry
   }
+  plen=(pbytes+3)&252; // No of bytes to allocate
   if((int32_t)((INTMEM_START+IMTMEM_SIZE) - mem_addr)<(plen+4)){
     return -2;  // Out of Memory
   }
+  uint8_t hdr[4]; 
+  hdr[2]=parid;
+  hdr[3]=pbytes;
 
-  crc=sdi_track_crc16(intmem_buf+2,plen+2,/*Init*/ 0);
-  *(uint16_t*)(intmem_buf)=crc; // Add CRC
-  nrf_nvmc_write_words(mem_addr, (uint32_t *)intmem_buf, qwords+1);
-  return (int16_t)(mem_addr-INTMEM_START);
+  uint16_t crc;
+  crc=sdi_track_crc16(hdr+2,2,/*Init*/ 0);  // First 2 Bytes
+  if(pbytes){
+    crc=sdi_track_crc16(pdata,pbytes,crc);  // Data Block
+  }
+  *(uint16_t*)(hdr)=crc; // Add CRC
+  nrf_nvmc_write_words(mem_addr, (uint32_t *)hdr, 1);
+  mem_addr+=4;
+  if(pbytes){
+    nrf_nvmc_write_words(mem_addr, (uint32_t *)pdata, plen/4);
+    mem_addr+=plen;
+  };
+  return (int16_t)(mem_addr-INTMEM_START); // Return End of Memory
 }
-// Read (last valid) Parameter to intmem_buf - Result 
-int16_t intpar_mem_read(uint8_t parid){
-  // todo
-  return 0; 
+
+// Read (last valid) Parameter to intmem_buf - Result Copy if pdata != NULL
+// If strings must be written: Optionally regard the trailing 0!
+int16_t intpar_mem_read(uint8_t parid, uint8_t *pdata){
+  uint32_t mem_addr;
+  mem_addr =  INTMEM_START;
+  int16_t res =-1;  // Not Found
+  uint8_t pbytes;
+  uint8_t mparid;
+  uint16_t plen;  // Len of Parameter Block in Bytes M4
+  uint16_t crc;
+  for(;;){
+    if ((*(uint32_t*)mem_addr)==0xFFFFFFFF) break;
+    if (mem_addr >= (INTMEM_START+IMTMEM_SIZE)) break;
+    pbytes=(*(uint8_t*)(mem_addr+3));
+    mparid=(*(uint8_t*)(mem_addr+2));
+    
+    if(mparid==parid){
+      crc=sdi_track_crc16((uint8_t*)(mem_addr+2),2+pbytes,/*Init*/ 0);  // First 2 Bytes
+      if(crc != *(uint16_t*)(mem_addr)){
+        res=-3; // Found, but CRC not OK for this Par
+      }else{
+        if(pdata){  // Move Parameter to Dest..
+          memcpy(pdata,(uint8_t*)(mem_addr+4), pbytes); // DSN
+        }
+        res=pbytes;
+      }
+    }
+    plen=(pbytes+3)&252;
+    mem_addr+= plen+4; // Next Entry
+  }
+  return res;
 }
 // Clear internal Memory
 void intpar_mem_erase(void){
@@ -201,9 +232,23 @@ bool type_service(void){
 
 // Die Input String und Values
 void type_cmdline(uint8_t isrc, uint8_t *pc, uint32_t val, uint32_t val2){
+
+  int res;
+
   switch(*pc){
   case 'm':
-    tb_printf("M: %d\n",intpar_mem_write(val,0,NULL)); // Testparameter schreiben, check mit 'H'
+    tb_printf("m: %d\n",intpar_mem_write(val,0,NULL)); // Testparameter schreiben, check mit 'H'
+    break;
+
+  case 'l':
+    tb_printf("l: %d\n",intpar_mem_write(val,strlen(pc+1),pc+1)); // Testparameter schreiben, check mit 'H'
+    break;
+
+  case 's':
+    res=intpar_mem_read(val,pc);
+    tb_printf("l: %d:",res);
+    if(res>0) for(int i=0;i<res;i++) tb_printf("%x ",pc[i]);
+    tb_printf("\n");
     break;
 
   case 'k':
